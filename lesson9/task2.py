2e8d44b664c4a4e6d168b54ccc7992ac55f8b250
Cubanych <sk-west@bk.ru>
2021-04-11 12:31:46 +0300

    Урок9.дз.2

--- lesson9/task2.py
+++ lesson9/task2.py
@@ -8,3 +8,19 @@
 # асфальтом, толщиной в 1 см * число см толщины полотна;
 # проверить работу метода.
 # Например: 20 м*5000 м*25 кг*5 см = 12500 т.
+
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 8     # автомобильная дорога: для дорог с интенсивным движением, каждый пласт должен быть не меньше 4 см, всего может быть от 2 до 4 слоев; 8 см.
+
+    def asphalt_mass(self):
+        asphalt_mass = self._length * self._width * self.weight * self.height/1000
+        print(f'Для покрытия всего дорожного полотна неободимо {round(asphalt_mass)} массы асфальта')
+
+
+r = Road(5000, 20)
+r.asphalt_mass()
